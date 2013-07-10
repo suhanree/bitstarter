53acab38da7977d1393675360f3768fc51675bb5
Suhan Ree <suhanree@yahoo.com>
2013-07-10 23:11:25 +0000

    Modified web.hs

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs=require('fs');
+var buf=new Buffer(100);
+buf=fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
